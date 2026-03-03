5be22af39dffb069dfa065fe98b2bf558a9d43d5
LegendCK <chiragkhairnar12@gmail.com>
2026-03-03 13:59:05 +0530

    Initial Commit

---
+++ RetailStoreManagementSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetailStoreManagementSystem
+//
+//  Created by admin56 on 03/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetailStoreManagementSystem/RetailStoreManagementSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetailStoreManagementSystemApp.swift
+//  RetailStoreManagementSystem
+//
+//  Created by admin56 on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RetailStoreManagementSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
